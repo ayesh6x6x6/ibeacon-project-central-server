52bba19e2000e204f3656f1ad1179680ed554249
Ayesh <atowheed@gmail.com>
2018-11-10 17:45:28 +0400

    Separation of powers

---
+++ models/user.js
@@ -0,0 +1,13 @@
+const mongoose = require('mongoose');
+
+const userSchema = mongoose.Schema({
+    // _id: mongoose.Schema.Types.ObjectId,
+    username: String,
+    email: String,
+    password: String,
+    favItems: [{ type: mongoose.Schema.Types.ObjectId, ref: 'MenuItem' }],
+    orderHistory: [{type: mongoose.Schema.Types.ObjectId, ref: 'Order'}],
+    preferredZone: [{type:String}]
+});
+
+module.exports = mongoose.model('User',userSchema);
