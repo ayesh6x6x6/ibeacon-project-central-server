18699cd779e685f694f9dc8660f14b482633742a
Ayesh <atowheed@gmail.com>
2018-11-14 18:38:00 +0400

    Update

--- auth/AuthController.js
+++ auth/AuthController.js
@@ -94,9 +94,11 @@ router.post('/login', function(req, res) {
   User.findOne({ email: req.body.email }, function (err, user) {
     if (err) return res.status(500).send('Error on the server.');
     if (!user) return res.status(404).send('No user found.');
+    console.log(user);
     if(user.password!=""){
       var passwordIsValid = bcrypt.compareSync(req.body.password, user.password);
       if (!passwordIsValid) return res.status(401).send({ auth: false, token: null });
+      console.log('Inside password');
     }
     var token = jwt.sign({ id: user._id }, config.secret, {
       expiresIn: 86400 // expires in 24 hours
