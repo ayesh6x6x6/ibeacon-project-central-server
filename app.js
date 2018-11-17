7b4925374db4b62ddd04a86f74d24f581a66c3ec
Ayesh <atowheed@gmail.com>
2018-11-17 18:15:36 +0400

    Modified getuser

--- app.js
+++ app.js
@@ -104,7 +104,7 @@ app.use('/api/auth', AuthController);
 var user = {};
 
 app.get('/api/getuser',(req,res)=>{
-    User.findOne({email:req.body.email},(err,userr)=>{
+    User.findOne({email:req.query.email},(err,userr)=>{
         if(err){
             console.log(err);
         } else {
